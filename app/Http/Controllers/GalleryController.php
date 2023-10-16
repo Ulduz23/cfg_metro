a634f37a58b4e90335613fd31892edb1bace9246
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-10-16 10:17:50 +0400

    gallery setup

---
+++ app/Http/Controllers/GalleryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreGalleryRequest;
+use App\Http\Requests\UpdateGalleryRequest;
+use App\Models\Gallery;
+
+class GalleryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreGalleryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateGalleryRequest $request, Gallery $gallery)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Gallery $gallery)
+    {
+        //
+    }
+}
