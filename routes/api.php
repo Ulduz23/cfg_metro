a5735c176a9525923b0fa650f3575a54c29b258b
Eldar Shahmaliyev <shahmal1yev@mail.ru>
2023-10-23 17:44:09 +0400

    delete

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use App\Http\Controllers\BannerController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\GalleryController;
